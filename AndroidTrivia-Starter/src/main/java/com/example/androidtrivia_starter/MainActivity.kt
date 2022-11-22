56c81f240748b5eb241a3e2f432e9aa644806aaf
wlsdn9297 <wlsdn9297@naver.com>
2022-11-22 15:41:39 +0900

    navigation drawer 추가

--- AndroidTrivia-Starter/src/main/java/com/example/androidtrivia_starter/MainActivity.kt
+++ AndroidTrivia-Starter/src/main/java/com/example/androidtrivia_starter/MainActivity.kt
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.androidtrivia_starter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         val navController = findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, binding.drawerLayout)
     }
 }
