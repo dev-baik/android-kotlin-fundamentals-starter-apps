a4c9bc1f806c9c74618099087c0c2846d93a9b67
wlsdn9297 <wlsdn9297@naver.com>
2022-11-22 11:36:36 +0900

    프래그먼트 추가

---
+++ AndroidTrivia-Starter/src/main/java/com/example/androidtrivia_starter/TitleFragment.kt
@@ -0,0 +1,17 @@
+package com.example.androidtrivia_starter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.androidtrivia_starter.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+    private lateinit var binding: FragmentTitleBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
+        binding = FragmentTitleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
