e6ad15817e717b799a4a858944b6bf33194e3907
wlsdn9297 <wlsdn9297@naver.com>
2022-11-22 12:59:48 +0900

    navigation graph 및 destination 설정

--- AndroidTrivia-Starter/src/main/java/com/example/androidtrivia_starter/TitleFragment.kt
+++ AndroidTrivia-Starter/src/main/java/com/example/androidtrivia_starter/TitleFragment.kt
@@ -1,10 +1,11 @@
 package com.example.androidtrivia_starter
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.androidtrivia_starter.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -12,6 +13,24 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
         binding = FragmentTitleBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.playButton.setOnClickListener {
+            findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 }
