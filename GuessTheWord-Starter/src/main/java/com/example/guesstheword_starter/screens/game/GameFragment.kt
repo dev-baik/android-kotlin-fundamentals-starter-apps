2403b6db7090a17bb2b776fd937706535077f72f
wlsdn9297 <wlsdn9297@naver.com>
2022-12-31 10:10:29 +0900

    게임 종료 버튼 클릭 리스너 구현

--- GuessTheWord-Starter/src/main/java/com/example/guesstheword_starter/screens/game/GameFragment.kt
+++ GuessTheWord-Starter/src/main/java/com/example/guesstheword_starter/screens/game/GameFragment.kt
@@ -57,6 +57,7 @@ class GameFragment : Fragment() {
 
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
+        binding.endGameButton.setOnClickListener { onEndGame() }
     }
 
     /** Methods for updating the UI **/
@@ -79,5 +80,16 @@ class GameFragment : Fragment() {
 
     private fun updateScoreText() {
         binding.scoreText.text = viewModel.score.toString()
+    }
+
+    private fun onEndGame() {
+        gameFinished()
+    }
+
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameToScore()
+        action.score = viewModel.score
+        NavHostFragment.findNavController(this).navigate(action)
     }
 }
